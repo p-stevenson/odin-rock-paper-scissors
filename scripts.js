f7e68d01aedcf9eddfd46fda1112d90f054287ab
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2021-11-19 15:42:20 +0000

    tidy up of comment

--- scripts.js
+++ scripts.js
@@ -15,37 +15,51 @@ function computerPlay() {
     // Math.floor will round down the value so the result is never greater than length.
 }
 
-
-// TODO tidy up pseudocode...
-//console.log(computerPlay());
+function capitalizeFirst(someString) {
+    return (someString[0].toUpperCase() + someString.substring(1));
+}
 
 // create const variable playerSelection = prompt('Rock, Paper, Scissors?')
+const playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
 // create const variable computerSelection = computerPlay()
-// make variables case insensitive.
+const computerSelection = computerPlay();
 // declare function called playRound()
 // function has two parameters playerSelection and computerSelection
-const playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-const computerSelection = computerPlay();
-
 function playRound(playerSelection, computerSelection) {
     
 // TODO consider if there is a simpler way to create win condition...
 //      move if to else if and add invalid input condition...
-    if (playerSelection === 'rock' && computerSelection === 'paper' ||
-        playerSelection === 'paper' && computerSelection === 'scissors' ||
-        playerSelection === 'scissors' && computerSelection === 'rock') {
-            return('You Lose! ' + computerSelection[0].toUpperCase() + computerSelection.substring(1) + ' beats ' + playerSelection[0].toUpperCase() + playerSelection.substring(1) + '.');
+//  if else statement to check for win condition.
+//  RPS logic
+//  first check if playerSelection is valid.
+    if 
+        (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+//          return invalid choice message            
+            alert('That is not a valid choice.');
+    }
+
+//  then check for a losing condition
+    else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+            playerSelection === 'paper' && computerSelection === 'scissors' ||
+            playerSelection === 'scissors' && computerSelection === 'rock') {
+//              return You Lose message if condition met.
+                alert('You Lose! ' + capitalizeFirst(computerSelection) + ' beats ' + capitalizeFirst(playerSelection) + '.');
         }
-        else {
-            return('You Win! ' + playerSelection[0].toUpperCase() + playerSelection.substring(1) + ' beats ' + computerSelection[0].toUpperCase() + computerSelection.substring(1) + '.');
+//  then check for tie condition.
+    else if (playerSelection === 'rock' && computerSelection === 'rock' ||
+            playerSelection === 'paper' && computerSelection === 'paper' ||
+            playerSelection === 'scissors' && computerSelection === 'scissors') {
+//              return it's a tie message.
+                alert('It\s a tie, choose again.');
+        }
+//          else return you win.
+    else {
+            alert('You Win! ' + capitalizeFirst(playerSelection) + ' beats ' + capitalizeFirst(computerSelection) + '.');
     }
     
 }
 
-console.log(playRound(playerSelection, computerSelection));
-// add logic for win/lose condition.
-// use logical operators to test for winning condition
-// return appropriate string based on outcome.
+playRound(playerSelection, computerSelection);
 
 
 
