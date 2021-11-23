abdd8bb79fd0c6fad3109d6ad3eb31fc97252e6b
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2021-11-23 13:00:26 +0000

    Rewrote majority of code.
    added function game()
    moved playRound() into game()
    rewrote game() do..while
    added code to check for win/lose condition and increment scores
    moved majority of variable out of global scope
    
    removed majority of unnecessary comments
    tidied up formatting in general

--- scripts.js
+++ scripts.js
@@ -1,65 +1,79 @@
-/*
-Declare a function called computerPlay
-This function will randomly return a string, either Rock, Paper or Scissors.
-This function will be used when it's the computers turn in our game.
-*/
+let playerSelection;
+let computerSelection;
 
-
-// create variable called options
-// assign array of strings to it.
-const options = ['Rock', 'Paper', 'Scissors'];
-// declare function computerPlay
 function computerPlay() {
-    // select and return item from array based on randomly generated number.
-    return options[Math.floor(Math.random() * options.length)].toLowerCase();
-    // Math.floor will round down the value so the result is never greater than length.
+    const options = ['rock', 'paper', 'scissors'];
+    // randomly select option from array.
+    return options[Math.floor(Math.random() * options.length)];
 }
 
-function capitalizeFirst(someString) {
+function capitalizeFirstLetter(someString) {
     return (someString[0].toUpperCase() + someString.substring(1));
 }
 
-// create const variable playerSelection = prompt('Rock, Paper, Scissors?')
-const playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-// create const variable computerSelection = computerPlay()
-const computerSelection = computerPlay();
-// declare function called playRound()
-// function has two parameters playerSelection and computerSelection
-function playRound(playerSelection, computerSelection) {
-    
-// TODO consider if there is a simpler way to create win condition...
-//      move if to else if and add invalid input condition...
-//  if else statement to check for win condition.
-//  RPS logic
-//  first check if playerSelection is valid.
-    if 
-        (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-//          return invalid choice message            
-            alert('That is not a valid choice.');
-    }
+// main game function
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let lose;
+    let win;
 
-//  then check for a losing condition
-    else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+    // logic for a single round
+    function playRound(playerSelection, computerSelection) {
+        computerSelection = computerPlay();
+        playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+        
+        if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+            alert('That is not a valid choice.');
+        }
+
+        else if (playerSelection === 'rock' && computerSelection === 'paper' ||
             playerSelection === 'paper' && computerSelection === 'scissors' ||
             playerSelection === 'scissors' && computerSelection === 'rock') {
-//              return You Lose message if condition met.
-                alert('You Lose! ' + capitalizeFirst(computerSelection) + ' beats ' + capitalizeFirst(playerSelection) + '.');
+            alert('You Lose! ' + capitalizeFirstLetter(computerSelection) + ' beats ' + capitalizeFirstLetter(playerSelection) + '.');
+            lose = true;
+            return lose;
         }
-//  then check for tie condition.
-    else if (playerSelection === 'rock' && computerSelection === 'rock' ||
+
+        else if (playerSelection === 'rock' && computerSelection === 'rock' ||
             playerSelection === 'paper' && computerSelection === 'paper' ||
             playerSelection === 'scissors' && computerSelection === 'scissors') {
-//              return it's a tie message.
-                alert('It\s a tie, choose again.');
+            alert('It\s a tie, choose again.');
         }
-//          else return you win.
-    else {
-            alert('You Win! ' + capitalizeFirst(playerSelection) + ' beats ' + capitalizeFirst(computerSelection) + '.');
+
+        else {
+            alert('You Win! ' + capitalizeFirstLetter(playerSelection) + ' beats ' + capitalizeFirstLetter(computerSelection) + '.');
+            win = true;
+            return win;
+        }
+
     }
-    
+
+    do {
+        playRound(playerSelection, computerSelection);
+        // checks for win or lose in playRound(), increments score and resets win/lose variable.
+        if (win) {
+            playerScore++;
+            win = null;
+            console.log('playerScore ' + playerScore);
+            if (playerScore === 5) {
+                alert('bleep boop...defeated? how can this be...brrrrpppp...');
+            }
+        }
+        else if (lose) {
+            computerScore++;
+            lose = null;
+            console.log('computerScore ' + computerScore);
+            if (computerScore === 5) {
+                alert('AH..HA..HA...kneel before me puny human...');
+            }
+
+        }
+    }
+
+    while (playerScore < 5 && computerScore < 5);
+
+
 }
 
-playRound(playerSelection, computerSelection);
-
-
-
+game();
