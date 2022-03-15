f2dd513e60ebeacb68d40e874cc12be7ed17be9d
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-03-15 16:23:58 +0000

    Add UI Buttons
    Add buttons to html.
    Remove prompts for user input.
    Move score counters to global scope.
    Add event listener for user choice via buttons.
    Temporarily remove game rounds loop logic.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,48 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     const option = ['rock', 'paper', 'scissors'];
     let computerSelection = option[Math.floor(Math.random() * option.length)];
     return computerSelection;
 }
 
-function playerPlay() {
-    let playerSelection = prompt('Choose your weapon...').toLowerCase();
-    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-        playerSelection = prompt(`${playerSelection} is not a valid option, try again.`).toLowerCase();
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection === playerSelection && computerSelection === computerSelection) {
+        case playerSelection === 'rock' && computerSelection === 'scissors':
+        case playerSelection === 'scissors' && computerSelection === 'paper':
+        case playerSelection === 'paper' && computerSelection === 'rock':
+            alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+            return (++playerScore);
+        case playerSelection === 'scissors' && computerSelection === 'rock':
+        case playerSelection === 'paper' && computerSelection === 'scissors':
+        case playerSelection === 'rock' && computerSelection === 'paper':
+            alert(`You lose...${computerSelection} beats ${playerSelection}`);
+            return (++computerScore);
+        case playerSelection === 'rock' && computerSelection === 'rock':
+        case playerSelection === 'paper' && computerSelection === 'paper':
+        case playerSelection === 'scissors' && computerSelection === 'scissors':
+            alert('It\'s a tie!, go again.');
+            break;
+        default:
+            alert('Sorry but you need to choose, Rock, Paper or Scissors');
+            break;
     }
-    return playerSelection;
 }
 
 function game() {
     let round = 0;
-    let playerScore = 0;
-    let computerScore = 0;
-    while (playerScore <= 3 && computerScore <= 3) {
+    const playersChoice = document.querySelectorAll('button');
+    playersChoice.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.value;
+            computerSelection = computerPlay();
+            playRound(playerSelection, computerSelection);
+            console.log(`Round ${++round}: Player ${playerScore} Computer ${computerScore}`);
+        });
+    });
+    /* while (playerScore <= 3 && computerScore <= 3) {
         playRound(playerPlay(), computerPlay());
-        console.log(`Round ${++round}: Player ${playerScore} Computer ${computerScore}`);
         if (playerScore === 3) {
             alert('Congratulations! you have defeated the computer.');
             break;
@@ -26,29 +50,8 @@ function game() {
             alert('Game Over');
             break;
         }
-    }
-    function playRound(playerSelection, computerSelection) {
-        switch (playerSelection === playerSelection && computerSelection === computerSelection) {
-            case playerSelection === 'rock' && computerSelection === 'scissors':
-            case playerSelection === 'scissors' && computerSelection === 'paper':
-            case playerSelection === 'paper' && computerSelection === 'rock':
-                alert(`You win! ${playerSelection} beats ${computerSelection}!`);
-                return (++playerScore);
-            case playerSelection === 'scissors' && computerSelection === 'rock':
-            case playerSelection === 'paper' && computerSelection === 'scissors':
-            case playerSelection === 'rock' && computerSelection === 'paper':
-                alert(`You lose...${computerSelection} beats ${playerSelection}`);
-                return (++computerScore);
-            case playerSelection === 'rock' && computerSelection === 'rock':
-            case playerSelection === 'paper' && computerSelection === 'paper':
-            case playerSelection === 'scissors' && computerSelection === 'scissors':
-                alert('It\'s a tie!, go again.');
-                break;
-            default:
-                alert('Sorry but you need to choose, Rock, Paper or Scissors');
-                break;
-        }
-    }
+    } */
 }
+
 
 game();
